e66f8fb5644ad11d4280f95f2dac2023e3b5bc73
Andery Rakov <rakov80@mail.ru>
2020-04-25 22:51:19 +0700

    изменён порт

--- main.py
+++ main.py
@@ -271,8 +271,7 @@ def main():
             abort(404)
         return redirect(f'/{0}/{0}')
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run('127.0.0.1', 8000, True)
 
 
 if __name__ == '__main__':
